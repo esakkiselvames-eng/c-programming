8f586b91ff3e81f1fc3a2d3b51817756a1bc18f5
esakkiselvames-eng <esakkiselvames@gmail.com>
2025-09-01 10:12:29 +0530

    Add files via upload

---
+++ selvam/Write a C Program to print a character in single quotes..cpp
@@ -0,0 +1,12 @@
+
+
+//Write a C Program to print a character in single quotes.
+#include<stdio.h>
+int main()
+{
+    char a ;
+	printf("Enter a character:");
+	scanf("%c''\n'",&a);
+	printf("%c'\n'",a);
+	return 0;
+}
---
+++ selvam/tab betwwen two numbers.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("Enter a number:");
+	scanf("%d %d",&num1,&num2);
+	printf("%d\t %d",num1,num2);
+	return 0;
+}
